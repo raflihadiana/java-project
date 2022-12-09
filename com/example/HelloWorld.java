104f0ccaae6092a01f069c8695cf9bd403668b22
raflihadiana <raflihadian24@gmail.com>
2022-12-09 17:32:04 +0700

    HelloWorld File

--- com/example/HelloWorld.java
+++ com/example/HelloWorld.java
@@ -2,6 +2,9 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String hello = "Halo Semuanya,";
+        int kemana = 7;
+
+        System.out.println(hello + " saya se" + kemana + " sama kamu!");
     }
 }
